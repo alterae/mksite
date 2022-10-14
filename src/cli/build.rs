219465b94eb8e9a2ffbdeab904e3d67fb1cf5acc
Michelle Szinger <alterae@protonmail.com>
2022-10-13 19:43:47 -0600

    Implement layout support

--- src/cli/build.rs
+++ src/cli/build.rs
@@ -41,6 +41,7 @@ pub(crate) fn cmd() -> anyhow::Result<()> {
             fs::create_dir_all(p)?;
         }
 
+        // FIXME: maybe use OsStrs in the config file?
         match path.extension().and_then(OsStr::to_str) {
             Some(ext) if config.transforms.contains_key(ext) => {
                 for (ext, proc) in &config.transforms[ext] {
@@ -49,17 +50,17 @@ pub(crate) fn cmd() -> anyhow::Result<()> {
                     println!("Transforming {path:?}...");
                     let output = proc.apply(output.as_bytes())?;
 
+                    let output = apply_layout(path, &output)?;
                     println!("     Writing {path:?}...");
                     fs::write(path, output)?;
                 }
             }
             _ => {
+                let output = apply_layout(&path, output.as_bytes())?;
                 println!("     Writing {path:?}...");
-                fs::write(path, output)?;
+                fs::write(&path, output)?;
             }
         }
-
-        // TODO: layouts
     }
 
     if Path::new(&config.dirs.r#static).exists() {
@@ -84,4 +85,56 @@ pub(crate) fn cmd() -> anyhow::Result<()> {
     }
 
     Ok(())
+}
+
+pub fn apply_layout(path: &Path, body: &[u8]) -> anyhow::Result<Vec<u8>> {
+    let config = config::load()?;
+    let stripped = path.strip_prefix(config.dirs.out)?;
+    let path = Path::new(&config.dirs.layout).join(stripped);
+    let wildcard = format!(
+        "_{}",
+        match path.extension().and_then(OsStr::to_str) {
+            Some(ext) => ".".to_owned() + ext,
+            None => "".to_owned(),
+        }
+    );
+
+    let layouts = Tera::new(
+        Path::new(&config.dirs.layout)
+            .join("**")
+            .join("*")
+            .to_str()
+            .unwrap(),
+    )?;
+
+    let layout = if path.exists() {
+        Some(stripped.to_owned())
+    } else {
+        let mut res = None;
+        for ancestor in path.ancestors() {
+            let path = ancestor.join(&wildcard);
+            res = if path.exists() {
+                Some(path.strip_prefix(&config.dirs.layout)?.to_owned())
+            } else {
+                None
+            };
+
+            if res.is_some() {
+                break;
+            }
+        }
+        res
+    };
+
+    if let Some(layout) = layout {
+        println!("    Applying layout {layout:?}...");
+        let mut context = tera::Context::new();
+        context.insert("content", &String::from_utf8(body.to_owned())?);
+
+        Ok(layouts
+            .render(layout.to_str().unwrap(), &context)?
+            .into_bytes())
+    } else {
+        Ok(body.to_owned())
+    }
 }
